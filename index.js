4fe37b522ffc43b93f5bfa4f3d7f7d2e96a082c0
unknown <mahfujakhanam023@gmail.com>
2024-12-12 15:35:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+// string data type
+const productName = "Mobile Phone";
+const productBrand = "Sumsang";
+let schoolName = "PGGSH";
+let shopName = "Fulghor";
+// Number data type
+let productPrice = 12000;
+let examMark = 90;
+const holdingNo = 252/2;
+let userPhoneNumber = 0123456789;
+const userNidNo = 0997564523;
+// Boolean data type
+const isBangladeshi = true;
+let first
+
+
+let firstNumber = 500;
+let secondNumber = 5;
+let devision = firstNumber / secondNumber;
+let percentage  = firstNumber % secondNumber;
+let modulus = firstNumber * secondNumber;
+
+
+let firstProduct = 3000;
+let secoundProduct = 4000;
+let thirdProduct = 5000;
+let totalPrice = (firstProduct + secoundProduct + thirdProduct);
+let discountPrice = (12000 / 10);
+console.log(discountPrice);
+
+// non primitive
+
+// array deta type
+let friendList = ['Shamim', 'Shakil', 'Sobuj', 'Sajal', 'Saim'];
+const age = [23, 24, 25, 26, 27];
+
+// object deta type
+let userProfile = {
+    id: 234556,
+    name: {
+        firstName : 'Kulsum',
+        lastName: 'Begum',
+    },
+    phoneNumber: 645456746,
+    address: 'Dhaka',
+    educational: 'Dhaka University',
+}
+console.log(friendList);
