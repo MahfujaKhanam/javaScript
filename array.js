7055feb4c365334fec0d49bc47224dae4d942a33
unknown <mahfujakhanam023@gmail.com>
2024-12-18 17:03:04 +0600

    new

---
+++ array.js
@@ -0,0 +1,9 @@
+let friends = ['shamim', 'seam', 'shakil'];
+let number = [10, 20, 30, 40];
+number.unshift(5)
+// friends.push('sumaiya');
+friends.pop();
+console.log(number);
+for(let i = 0; i< number.length; i++){
+    console.log(number)
+}
