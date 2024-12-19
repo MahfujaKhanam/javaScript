2de45dad58e29c42f4dd429f6ce85248204028da
unknown <mahfujakhanam023@gmail.com>
2024-12-19 12:35:36 +0600

    new

--- array.js
+++ array.js
@@ -1,9 +1,76 @@
-let friends = ['shamim', 'seam', 'shakil'];
-let number = [10, 20, 30, 40];
-number.unshift(5)
-// friends.push('sumaiya');
-friends.pop();
-console.log(number);
-for(let i = 0; i< number.length; i++){
-    console.log(number)
-}
+// let friends = ['shamim', 'seam', 'shakil'];
+// let number = [10, 20, 30, 40];
+// number.unshift(5)
+// // friends.push('sumaiya');
+// friends.pop();
+// console.log(number);
+// for(let i = 0; i< number.length; i++){
+//     console.log(number);
+// }
+
+
+// arrray at method
+// let number = [10, 20, 30, 40, 50];
+// let num = number.at(3);
+// console.log(num)
+
+
+// arrray delete method
+// let number = [10, 20, 30, 40, 50];
+// let num = delete number[4];
+// console.log(num);
+
+
+// arrray flat method
+// let number = [[10, 20], [30,40], [50, 60]];
+
+// let num = number.flat();
+// console.log(num);
+
+
+// arrray concat method
+// let number = [10, 20, 30, 40,];
+
+// let con = number.concat(50);
+// console.log(con);
+
+
+// arrray splice method
+// let number = [10, 20, 30, 40,];
+
+// let spl = number.splice(20);
+// console.log(spl);
+
+
+// arrray slice method
+// let number = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120];
+
+// let sli = number.slice(2, 4);
+// console.log(sli);
+
+
+// arrray index method
+// let number = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120];
+
+// let ind = number.indexOf(60);
+// console.log(ind);
+
+
+// arrray lastindexof method
+// let number = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120];
+
+// let lastin = number.lastIndexOf(70) +1;
+// console.log(lastin);
+
+
+
+// arrray includes method
+// let number = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120];
+// let inc = number.includes(170);
+// console.log(inc);
+
+
+// arrray includes method
+let number = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120];
+let inc = number.find(40);
+console.log(inc);
