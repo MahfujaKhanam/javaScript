e2e64783ca3b73d18f1236190dc2785c122ecc4c
unknown <mahfujakhanam023@gmail.com>
2024-12-24 12:07:33 +0600

    new

---
+++ usersarray.js
@@ -0,0 +1,82 @@
+const user = [
+    {
+        id: 1,
+        name: 'Mursheda',
+        age: 22,
+        email: 'mursheda@gmail.com',
+        phone: '5565464564',
+        address: {
+            street: '123 Main St',
+            city: 'New York',
+            state: 'NY',
+            zip: '34355'
+        },
+        occupation: 'Engineer',
+        hobbies:['reading', 'traveling']
+    },  
+    {
+        id: 2,
+        name: 'Mursheda',
+        age: 22,
+        email: 'mursheda@gmail.com',
+        phone: '5565464564',
+        address: {
+            street: '123 Main St',
+            city: 'New York',
+            state: 'NY',
+            zip: '34355'
+        },
+        occupation: 'Engineer',
+        hobbies:['reading', 'traveling']
+    },
+    {
+        id: 3,
+        name: 'Mursheda',
+        age: 22,
+        email: 'mursheda@gmail.com',
+        phone: '5565464564',
+        address: {
+            street: '123 Main St',
+            city: 'New York',
+            state: 'NY',
+            zip: '34355'
+        },
+        occupation: 'Engineer',
+        hobbies:['reading', 'traveling']
+    },
+    {
+        id: 4,
+        name: 'Mursheda',
+        age: 22,
+        email: 'mursheda@gmail.com',
+        phone: '5565464564',
+        address: {
+            street: '123 Main St',
+            city: 'New York',
+            state: 'NY',
+            zip: '34355'
+        },
+        occupation: 'Engineer',
+        hobbies:['reading', 'traveling']
+    },
+    {
+        id: 5,
+        name: 'Mursheda',
+        age: 22,
+        email: 'mursheda@gmail.com',
+        phone: '5565464564',
+        address: {
+            street: '123 Main St',
+            city: 'New York',
+            state: 'NY',
+            zip: '34355'
+        },
+        occupation: 'Engineer',
+        hobbies:['reading', 'traveling']
+    },
+];
+for(i = 0; i < user.length; i++){
+    console.log(user[i]);
+}
+// console.log(user[0].address.city);
+// console.log(user[0].hobbies);
